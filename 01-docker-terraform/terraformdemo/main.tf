9a71ac6a199f6616dfb3f1f3115c934bc350c7ab
Muhammad Ibrahim <116669316+Muhammadibra40@users.noreply.github.com>
2025-12-29 09:41:40 +0000

    Sync

--- 01-docker-terraform/terraformdemo/main.tf
+++ 01-docker-terraform/terraformdemo/main.tf
@@ -8,6 +8,35 @@ terraform {
 }
 
 provider "google" {
-  project     = "my-project-id"
+  credentials = "${file("keys/terrademo-credits.json")}"
+  project     = "airy-sled-482514-m7"
   region      = "us-central1"
+}
+# bucket with lifecycle rules
+# bucket variable name: first-demo-bucket 
+# "defines the name of the resource in terraform, doesn't need to be globally unique"
+# bucket name: airy-sled-482514-m7-terraform-demo-bucket
+# "defines the name of the bucket in GCP, needs to be globally unique"
+resource "google_storage_bucket" "first-demo-bucket" {
+  name          = "airy-sled-482514-m7-terraform-demo-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
 }
